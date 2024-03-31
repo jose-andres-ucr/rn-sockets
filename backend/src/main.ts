51d7d2d0c19ff80c8f0f0aaae6e396fd3fa3571a
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2024-03-31 16:17:11 -0600

    Sockets

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import os from "node:os";
+import http from "node:http";
+import { Server } from "socket.io";
 
 // Obtener mi IP privado
 function getPrivateIP() {
@@ -14,10 +16,8 @@ function getPrivateIP() {
   return "0.0.0.0";
 }
 
+// REST
 const app = express();
-const port = 5000;
-const host = getPrivateIP();
-
 app.get("/", (req, res) => {
   res.send({
     message: "Qué dicha que hay vacaciones",
@@ -25,6 +25,26 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, host, () => {
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+// Sockets
+io.on("connection", (socket) => {
+  console.log("A user is connected");
+
+  socket.on("message", (msg) => {
+    console.log("BREAKPOINT", msg);
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("One user left");
+  });
+});
+
+const port = 5000;
+const host = getPrivateIP();
+server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,49 +1,71 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import Constants from "expo-constants";
-import pTimeout from "p-timeout";
+import io from "socket.io-client";
 
-const getText = async () => {
+const getIp = () => {
   // Para obtener la IP de la red actual, para no copiar y pegar la IP
   const debuggerHost = Constants.expoConfig?.hostUri;
-  const ip = debuggerHost?.split(":")[0];
-
-  try {
-    const result = await pTimeout(fetch(`http://${ip}:5000`), {
-      milliseconds: 2000,
-    });
-
-    if (result.ok) {
-      const resultJson = await result.json();
-      return resultJson.message;
-    }
-    return "Not ok error";
-  } catch (error) {
-    return "Error catch";
-  }
+  return debuggerHost?.split(":")[0] ?? "";
 };
 
+const socket = io(`http://${getIp()}:5000`); // Server address
+
+const authorId = Math.round(Math.random() * 100);
+
 export default function App() {
-  const [apiText, setApiText] = React.useState("");
+  const [input, setInput] = React.useState("");
+  const [socketMessages, setSocketMessages] = React.useState(
+    [] as { author: number; text: string }[]
+  );
+
+  const sendMessage = () => {
+    // Sending a message
+    if (input.trim() !== "") {
+      socket.emit("message", { author: authorId, text: input });
+      setInput("");
+    }
+  };
 
   React.useEffect(() => {
-    const getExample = async () => {
-      setApiText(await getText());
-    };
+    // Listen to messages from the server
+    socket.on("message", (msg) => {
+      setSocketMessages((prev) => [...prev, msg]);
+    });
 
-    void getExample();
+    return () => {
+      socket.disconnect(); // Close socket connection when component unmount
+    };
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <Text>Texto desde API: {apiText}</Text>
-      <Button
-        title="Refresh"
-        onPress={async () => setApiText(await getText())}
-      />
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", padding: 50 }}>
+      <ScrollView style={{ flex: 1, marginBottom: 16 }}>
+        {socketMessages.map((msg, index) => (
+          <Text key={index}>{`Author: ${msg.author} - ${msg.text}`}</Text>
+        ))}
+      </ScrollView>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <TextInput
+          style={{
+            flex: 1,
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            marginRight: 8,
+          }}
+          onChangeText={(text) => setInput(text)}
+          value={input}
+        />
+        <Button title="Send" onPress={sendMessage} />
+      </View>
     </View>
   );
 }
